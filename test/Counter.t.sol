885c4d13fbdeb71b08e96f07a734f0e78d252a33
Xiang Cui <xc1008@nyu.edu>
2023-04-14 16:22:38 -0400

    add poc

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "forge-std/console.sol";
+
+import "../src/Mock/MockNFT.sol";
+import "../src/Mock/MockERC20.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testNormal() public {
+
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testExploit() public {
+        console.log("exploit");
     }
+
 }
